62c26a4e9a9c3fc179bc19f30ce2a36295efac32
Vishnu Sonawane <vidya.vishnusonawane@philips.com>
2019-07-10 22:04:15 +0530

    Add project files.

---
+++ MyConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyDll;
+
+namespace MyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Vidya from myconsole.!!!");
+            Class1 cls = new Class1();
+            Console.WriteLine(cls.method());
+        }
+    }
+}
